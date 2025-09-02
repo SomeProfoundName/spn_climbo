fefcf362aba00482fa95c96087e64f55dca11be0
someprofoundname <collinscomondi@gmail.com>
2025-09-03 00:07:37 +0100

    add nav

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,29 @@
+import { Tabs } from 'expo-router'
+import React from 'react'
+
+const _layout = () => {
+  return (
+    <Tabs>
+        <Tabs.Screen 
+        name='index'
+        options={{
+            title: "Home",
+            headerShown: false
+        }}/>
+        <Tabs.Screen 
+        name='climbs'
+        options={{
+            title: "Log Climb",
+            headerShown: false
+        }}/>
+        <Tabs.Screen 
+        name='profile'
+        options={{
+            title: "Profile",
+            headerShown: false
+        }}/>
+    </Tabs>
+  )
+}
+
+export default _layout
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,12 @@
 import { Stack } from "expo-router";
 import "./global.css";
+import { ScreenStack } from "react-native-screens";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack>
+    <Stack.Screen 
+    name="(tabs)"
+    options={{headerShown: false}}
+    />
+    </Stack>;
 }
